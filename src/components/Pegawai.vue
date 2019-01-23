41d26dc3a97abb20ed82b9caec830a3c507a6326
Nur Zaenudin <nurzaenudin@kemenkeu.go.id>
2019-01-23 15:10:02 +0700

    "menambah axios dan get sudah bisa"

---
+++ src/components/Pegawai.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <table border="1px">
+            <thead>
+                <td>No</td>
+                <td>Nama</td>
+                <td>NIP</td>
+            </thead>
+            <tbody>
+                <tr v-for="datapegawai in datapegawais">
+                    <td>Isi No</td>
+                    <td>{{datapegawai.nama}}</td>
+                    <td>{{datapegawai.nip}}</td>
+                </tr>
+            </tbody> 
+        </table>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data: function(){
+        return {
+            datapegawais:[]
+        };      
+    },
+
+    created: function (){
+        axios
+            .get('http://localhost:8090/pegawai')
+            .then(response=>{this.datapegawais=response.data})
+            .catch(e => {this.errors.push(e)
+    })
+    }
+}
+</script>
+
